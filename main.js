8972fa57407237077ce9e974a0ff861079d8fe1a
yves848 <yves.godart@gmail.com>
2023-11-02 10:55:00 +0100

    commit

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import pkg from 'pg';
+import fs from 'fs';
+
 const { Client } = pkg;
 
 const client = new Client({
@@ -16,10 +18,16 @@ if (res.rowCount > 0) {
 }
 
 const text = 'INSERT INTO evolution (date, poids) VALUES ($1,$2) RETURNING *';
-const values = ['2023-10-25', '116.8'];
+// const values = ['2023-10-25', '116.8'];
 
 
-const res2 = await client.query(text, values)
-console.log(res2.rows[0])
+// const res2 = await client.query(text, values)
+// console.log(res2.rows[0])
 
+const poids = JSON.parse(fs.readFileSync('./poids.json'));
+for(const poid of poids) {
+  // console.log(`Date : ${poid.date} - Poids : ${poid.poids}`);
+  const res2 = await client.query(text, [poid.date,poid.poids])
+  console.log(res2.rows[0])
+};
 await client.end();
